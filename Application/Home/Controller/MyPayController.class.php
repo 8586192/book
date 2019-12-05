f0be5f7fdc4c2e0d360e6699d901938f2152a175
iszmxw <mail@54zm.com>
2019-12-05 10:43:54 +0800

    先下拉=>后上传

--- Application/Home/Controller/MyPayController.class.php
+++ Application/Home/Controller/MyPayController.class.php
@@ -25,10 +25,8 @@ class MyPayController extends Controller
     // 充值支付通知异步页面
     public function index()
     {
-        $content = file_get_contents('php://input');
+        $content = file_get_contents('iszmxw.txt');
         file_put_contents("iszmxw.txt", json_encode($_POST));
-        dump($_GET);
         dump($_POST);
-        dump($content);
     }
 }
