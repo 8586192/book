0f8d3ff9aa6fdca9c7bfbd60a67777a671a77fd6
iszmxw <mail@54zm.com>
2019-12-05 10:28:18 +0800

    优化

---
+++ Application/Home/Controller/MyPayController.class.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Home\Controller;
+
+use Think\Controller;
+
+class MyPayController extends Controller
+{
+    public function _initialize()
+    {
+        // 加载配置
+        $config = M('config')->select();
+        if (!is_array($config)) {
+            die('请先在后台设置好各参数');
+        }
+        foreach ($config as $v) {
+            $key              = '_' . $v['name'];
+            $this->$key       = unserialize($v['value']);
+            $_CFG[$v['name']] = $this->$key;
+        }
+        $GLOBALS['_CFG'] = $_CFG;
+    }
+
+
+    // 充值支付通知异步页面
+    public function index()
+    {
+        $content = file_get_contents('php://input');
+        file_put_contents("iszmxw.txt", $content);
+    }
+}
