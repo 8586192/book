57a9774cd946f2bf1318734c5bacbd23d185c8aa
追梦小窝 <mail@54zm.com>
2019-12-09 18:20:50 +0800

    先下拉=>后上传

--- Application/Home/Controller/HomeController.class.php
+++ Application/Home/Controller/HomeController.class.php
@@ -163,9 +163,7 @@ class HomeController extends Controller
         $this->assign('user', $this->user);
         $this->_data_log();
         $showAds = 0;
-        dump($this);
-        die();
-        if (1 == $this->_ads['isopen']) {
+        if (isset($this->_ads['isopen']) && 1 == $this->_ads['isopen']) {
             $jino = I('get.ji_no');
             if ($jino) {
                 if ($this->_ads['chapter']) {
