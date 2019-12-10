541ac526cc281be14445de6a67e9f37bf0014d61
iszmxw <mail@54zm.com>
2019-12-10 23:41:50 +0800

    服务器提交

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit7f38a7fae9cb35844cbd78e090dfff5d
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit7f38a7fae9cb35844cbd78e090dfff5d::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire7f38a7fae9cb35844cbd78e090dfff5d($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire7f38a7fae9cb35844cbd78e090dfff5d($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
